9db085a4595e9f764800c66dfd4492d7c24e4ac7
namneet8 <namneettung123@gmail.com>
2025-01-15 01:10:36 -0500

    Completed Day 5

---
+++ Lesson 5 Loops/1_ForLoops.py
@@ -0,0 +1,17 @@
+# Syntax
+# for <variable name of each item> in <a List>:
+#     <do something>
+#     <do something else>
+
+# Predict what will be printed from the code below:
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+
+# Apple
+# Apple pie
+# Peach
+# Peach pie
+# Pear
+# Pear pie
